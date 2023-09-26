719e2bb1932299108afbcf9fb6ca14f047b32d81
Vansh <95956358+VRRANSH@users.noreply.github.com>
2023-09-26 11:59:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,40 @@
-1
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LoginSignupform from './components/loginandsignup.jsx';
+import "./styles/loginandsignup.css"
+import Nav from './components/nav';
+import "./App.css"
+import Chatbody from './components/chatbody';
+import RingLoader from "react-spinners/RingLoader";
+import { useState, useEffect } from 'react';
+
+function App() {
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 5000)
+    }, [])
+    return (
+        <>
+            {
+                loading ? <RingLoader
+                text="Loader Text"
+                animation="clock"
+                color="#FFF"
+                /> :
+
+                    <Router>
+                        <Routes>
+                            <Route path="/" element={<Nav />} />
+                            <Route path="/nav" element={<LoginSignupform />} />
+                            <Route path="/chats" element={<Chatbody />} />
+
+                        </Routes>
+                    </Router>
+            }
+        </>
+    )
+}
+
+export default App;
